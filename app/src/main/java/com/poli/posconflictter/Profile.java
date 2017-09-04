eb721d343e7590ad17102f62697cd7ef67852703
Camilo D'Achiardi <cami.a95@me.com>
2017-09-04 01:14:55 -0500

    Menú Principal beta #56

--- app/src/main/java/com/poli/posconflictter/Profile.java
+++ app/src/main/java/com/poli/posconflictter/Profile.java
@@ -1,8 +1,43 @@
 package com.poli.posconflictter;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 /**
  * Created by iGabetoPC on 21/08/2017.
  */
 
-public class Profile {
+public class Profile  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_event);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.barra_menu);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.event:
+                setContentView(R.layout.fragment_event);
+                return true;
+
+            case R.id.museum:
+                setContentView(R.layout.fragment_museum);
+                return true;
+
+            case R.id.profile:
+                setContentView(R.layout.fragment_profile);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }
